0102b814a12eae3ccf6d6b29cd1b2403bf5b7b62
Alexander Vogel <50327123+vogel-alex@users.noreply.github.com>
2026-05-28 14:52:02 +0000

    pseudo provider

--- src/main/java/home/automation/switches/SwitchProvider.java
+++ src/main/java/home/automation/switches/SwitchProvider.java
@@ -1,7 +1,11 @@
 package home.automation.switches;
 
+import java.util.List;
+
 public class SwitchProvider {
 
-    
+    List<SwitchCollection> getCollections(){
+        return List.of();
+    }
 
 }
